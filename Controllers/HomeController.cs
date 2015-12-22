4ae5901c211b051a9c8d9893c5d1558b6c4ffe2d
jill <jill@epicodus.com>
2015-12-21 16:20:28 -0800

    Add initial files with working database connection

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using EmptyMVCAuthentication01.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmptyMVCAuthentication01.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDbContext db = new MyDbContext();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return View("TestView");
+            }
+                
+        }
+        [HttpPost]
+        public IActionResult Index(TestModel testmodel)
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TestModel testmodel)
+        {
+            try
+            {
+                db.TestModels.Add(testmodel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+
+namespace EmptyMVCAuthentication01.Models
+{
+    public class MyDbContext : DbContext
+    {
+        
+        public virtual DbSet<TestModel> TestModels { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=EmptyMVCAuthentication01;integrated security=True;");
+        }
+    }
+}
